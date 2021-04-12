c9ff89a57437dcf7eae973aab0fa1643820b5858
luoben136 <82363469+luoben136@users.noreply.github.com>
2021-04-12 17:32:05 +0800

    Summation Notes
    Summation Notes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include<iostream>
 using namespace std;
-
-int main()
+int main()				//主函数
 {
-	cout<< "int    占的字节数是：" << sizeof(int) << "个字节" << endl;
-	cout<< "short  占的字节是：" << sizeof(short) << "个字节" << endl;
-	cout<< "long   占的字节是：" << sizeof(long) << "个字节" << endl;
-	cout<< "char   占的字节是：" << sizeof(char) << "个字节" << endl;
-	cout<< "float  占的字节是：" << sizeof(float) << "个字节" << endl;
-	cout<< "double 占的字节是：" << sizeof(double) << "个字节" << endl;
+	int i,sum=0;		//定义变量
 
-	getchar();
-	return 0;
+	for(i=1;i<=10;i++)	//循环语句,i从1到10循环
+	{
+		sum+=i;			//sum=sum+i
+	}
+
+	cout<< "sum=" << sum << endl;//输出sum的最终值
+
+	getchar();			//键盘上读入一个字符，并带回显
+	return 0;			//返回值为0，对于int类型
 }
+
+/*#include<iostream>
+using namespace std;
+int main()				//主函数
+{
+	int i=1, sum=0;		//定义变量
+
+	do					//循环语句
+	{
+		sum+=i;			//sum=sum+i
+		i++;			//i从1开始循环
+	}while(i<=10);		//i的循环条件小于等于10
+
+	cout<< "sum=" << sum << endl;//输出sum的最终值
+
+	getchar();			//键盘上读入一个字符，并带回显
+	return 0;			//返回值为0，对于int类型
+}*/
