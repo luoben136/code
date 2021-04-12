b86564e410dc2066263a30d4dc7248a8d5016a4d
luoben136 <82363469+luoben136@users.noreply.github.com>
2021-04-12 17:30:57 +0800

    Number of bytes displayed
    Display the number of bytes of the data type

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main()
 {
-	int a;
-
-	cout<< "Please input a number" << endl;
-	cin>> a;
-	cout<< "This number is " << a << endl;
+	cout<< "int    占的字节数是：" << sizeof(int) << "个字节" << endl;
+	cout<< "short  占的字节是：" << sizeof(short) << "个字节" << endl;
+	cout<< "long   占的字节是：" << sizeof(long) << "个字节" << endl;
+	cout<< "char   占的字节是：" << sizeof(char) << "个字节" << endl;
+	cout<< "float  占的字节是：" << sizeof(float) << "个字节" << endl;
+	cout<< "double 占的字节是：" << sizeof(double) << "个字节" << endl;
 
 	getchar();
 	return 0;
